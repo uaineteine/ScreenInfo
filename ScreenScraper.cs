db3d59097c2132e9700e4125b4e050d70737c365
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2020-09-26 14:11:06 +1000

    Add project files.

---
+++ ScreenScraper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Coord;
+
+namespace ScreenScraper
+{
+    public class ScreenInfo
+    {
+        public ScreenInfo(coord UpperLeftPos, coord Size)
+        {
+            size = Size;
+            pos = UpperLeftPos;
+        }
+        public coord size;
+        public coord pos;
+    }
+    public static class scraper
+    {
+        public static int NoMonitors()
+        {
+            return Screen.AllScreens.Length;
+        }
+        public static ScreenInfo[] GetScreenInfo()
+        {
+            int n = NoMonitors();
+            ScreenInfo[] info = new ScreenInfo[n];
+            for (int i = 0; i < n; i++)
+            {
+                info[i] = GetScreen(i);
+            }
+            return info;
+        }
+        public static ScreenInfo GetScreen(int screenNo)
+        {
+            Rectangle bounds = Screen.AllScreens[screenNo].Bounds;
+            return new ScreenInfo(new coord(bounds.X, bounds.Y), new coord(bounds.Width, bounds.Height));
+        }
+    }
+}
